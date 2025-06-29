395e4aaadb2613706f68943570373ebcd60d942b
Sergio Leonardo Murua <sergioleonardomurua@gmail.com>
2025-06-28 21:26:00 -0300

    se hizo la totalidad de los cambios en sobre mi dejandolo listo, con corroboraciones y estilo sobrio

--- core/forms.py
+++ core/forms.py
@@ -2,11 +2,26 @@ from django import forms
 from .models import Contenido
 
 class ContactoFormulario(forms.Form):
-    nombre = forms.CharField(max_length=100, label='Tu nombre')
-    email = forms.EmailField(label='Correo electrónico')
-    mensaje = forms.CharField(widget=forms.Textarea, label='Que me quieres decir?')
+    nombre = forms.CharField(
+        max_length=100,
+        label='Tu nombre',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        label='Correo electrónico',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    mensaje = forms.CharField(
+        label='¿Qué me quieres decir?',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 5,
+            'placeholder': 'Escribí tu mensaje acá...'
+        })
+    )
 
 class ContenidoForm(forms.ModelForm):
     class Meta:
         model = Contenido
         fields = ['titulo', 'descripcion', 'imagen', 'texto_completo']
+
